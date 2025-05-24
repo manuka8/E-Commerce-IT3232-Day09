cd6b6e676462e5357f418660772350b83c47bda9
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-05-24 09:41:41 +0530

    Create error response

---
+++ day9/src/main/java/lk/ac/vau/fas/ict/model/ErrorResponse.java
@@ -0,0 +1,29 @@
+package lk.ac.vau.fas.ict.model;
+
+public class ErrorResponse {
+	private int statusCode;
+	private String errorMessage;
+	public ErrorResponse() {
+		
+	}
+	public ErrorResponse(int statusCode, String errorMessage) {
+		super();
+		this.statusCode = statusCode;
+		this.errorMessage = errorMessage;
+	}
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+	
+	
+}
