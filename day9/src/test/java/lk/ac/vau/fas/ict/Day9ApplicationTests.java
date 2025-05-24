894ad91ec0d3629be37c49c2a7e4628a8038818e
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-05-24 09:08:16 +0530

    Intializing day 9 project

---
+++ day9/src/test/java/lk/ac/vau/fas/ict/Day9ApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.vau.fas.ict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
